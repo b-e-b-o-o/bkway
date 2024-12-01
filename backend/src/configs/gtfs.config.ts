910154c914a9ad2144ee12f362e0dbce3df48e63
MicrosoftEggshell <szgmate2002@gmail.com>
2024-12-01 17:44:11 +0100

    :)

--- backend/src/configs/gtfs.config.ts
+++ backend/src/configs/gtfs.config.ts
@@ -3,10 +3,10 @@ export default {
   agencies: [
     {
       // Production database URL:
-      // url: "https://go.bkk.hu/api/static/v1/public-gtfs/budapest_gtfs.zip",
+      url: "https://go.bkk.hu/api/static/v1/public-gtfs/budapest_gtfs.zip",
 
       // Test database location:
-      path: "./test/gtfs_sample.zip"
+      // path: "./test/gtfs_sample.zip"
     }
   ]
 };
